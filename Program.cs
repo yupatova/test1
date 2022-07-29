6c2ffcbdde6661810cedb3340fdcc92118bcc4fb
Yupatova Anna <yupatova@gmail.com>
2022-07-30 04:51:47 +0500

    добавила код решения задачи

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿void WordsThree (string[] mass1, string[] mass2)
+{
+    int count = 0;
+    for (int i = 0; i < mass1.Length; i++)
+    {
+    if(mass1[i].Length <= 3)
+        {
+        mass2[count] = mass1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write (mass[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string [] words = {"hello", "2", "world", ":-)"};
+string [] newWords = new string[words.Length];
+
+WordsThree(words, newWords);
+
+PrintArray(words);
+PrintArray(newWords);
